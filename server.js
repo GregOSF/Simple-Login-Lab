d8332d743caf3163064b14c3907d7c938f9b51a5
GregOSF <gregonza1@MacBook-Bro.local>
2015-07-15 23:03:04 -0700

    Add route to log in users, set up sessions and session/login page

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var express = require('express'),
   ejs = require('ejs'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
-  User = require('./models/user');
+  User = require('./models/user'),
+  session = require('express-session');
 
 // connect to mongodb
 mongoose.connect('mongodb://localhost/simple_login')
@@ -17,9 +18,49 @@ app.set('view engine', 'ejs');
 // middleware
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(session ({
+	saveUninitialized: true,
+	resave: true,
+	secret: 'SuperSecretCookie',
+	cookie: {maxAge: 6000}
+}));
+
+// middleware to manage sessions
+app.use ('/', function (req, res, next) {
+	// saves userID in session for logged in user
+	req.login = function (user) {
+		req.session.userID = user.id;
+	};
+	// finds user currently logged in based on 'session.userId'
+	req.currentuser = function (callback) {
+		User.findOne({_id: req.session.userId}, function (err, user) {
+			req.user - user;
+			callback(null, user);
+		});
+	};
+	//destroy 'session.userId' to log out user
+	req.logout - function () {
+		req.session.userId = null;
+		req.user = null;
+	};
+	next();
+});
+
+
+
+
+
 // signup route with placeholder response
 app.get('/signup', function (req, res) {
   res.send('coming soon');
+});
+
+// user profile page
+app.get('/profile', function (req, res) {
+	// finds user currently logged in
+	req.currentuser (function (err, user) {
+		res.send('Welcome ' + user.email);
+	});
 });
 
 // user submits the signup form
@@ -32,8 +73,21 @@ app.post ('/users', function (req, res) {
 	User.createSecure(newUser.email, newUser.password, function (err, user) {
 		res.send(user);
 	});
-}),
+});
 
+// user submits the login form
+
+app.post('/login', function (req, res) {
+	//grab user data from params (req.body)
+	var userData = req.body.user;
+	// call authenticate function to check if password user entered is correct
+	User.authenticate(userData.email, userData.password, function (err, user) {
+		// saves user id to session
+		res.login(user);
+
+		res.redirect('/profile');
+	});
+});
 
 // listen on port 3000
 app.listen(3000, function () {
